669c04a524b980ce3de1d0b5fc4a46d402fb6cb8
Jiho Park <j.park28@hotmail.com>
2022-03-27 19:35:10 -0600

    first test

---
+++ sketch.js
@@ -0,0 +1,144 @@
+
+// Circle Objects
+var circle_1;
+var circle_2;
+var mouseCircle;
+
+var radiusLimit = 80;
+
+var circles = [];
+
+
+
+class Circle{
+  constructor(radius, horizontal_speed, vertical_speed, color) {
+    this.radius = radius;
+    this.horizontal_speed = horizontal_speed;
+    this.vertical_speed = vertical_speed;
+    this.xaxis = random(0 + this.radius, windowWidth - this.radius);
+    this.yaxis = random(0 + this.radius, windowHeight - this.radius);
+    this.color = color
+  }
+
+  create(){
+    strokeWeight(4);
+    stroke(0, 0, 0);
+    fill(this.color);
+    ellipse(this.xaxis, this.yaxis, this.radius);
+  }
+
+  move(){
+    this.xaxis = this.xaxis + this.horizontal_speed;
+    this.yaxis = this.yaxis + this.vertical_speed;
+  }
+
+  bounceRandomly(){
+
+    if (this.xaxis + this.radius / 2 > windowWidth || this.xaxis - this.radius / 2 < 0){
+      this.horizontal_speed = this.horizontal_speed * -1;
+    }
+
+    if (this.yaxis + this.radius / 2 > windowHeight || this.yaxis - this.radius / 2 < 0){
+      this.vertical_speed = this.vertical_speed * -1;
+    }
+  }
+
+  increaseSize(){
+    this.radius += 1;
+  }
+
+  disappear(){
+    this.radius = 0;
+  }
+
+  
+
+  
+}
+
+
+
+
+function setup() {
+  
+  createCanvas(windowWidth, windowHeight);
+
+
+  circle_1 = new Circle(3, 6, 6, ('yellow'));
+  circle_2 = new Circle(25, 6, 6, ('lime'));
+  circle_3 = new Circle(50, 6, 6, ('pink'));
+  circle_4 = new Circle(30, 6, 6, ('blue'));
+  circle_5 = new Circle(60, 6, 6, ('white'));
+
+  circles.push(circle_1);
+  circles.push(circle_2);
+  circles.push(circle_3);
+  circles.push(circle_4);
+  circles.push(circle_5);
+
+}
+
+function draw() {
+
+  background(105,105,105);
+
+  console.log(circles.length);
+
+  // circles[0].create();
+  // circles[1].create();
+  // circles[2].create();
+  // circles[3].create();
+  // circles[4].create();
+
+  
+
+  for(let i = 0; i < circles.length; i++){
+    circles[i].create();
+    if (circles[i].radius < radiusLimit){
+      circles[i].increaseSize();
+    } else {
+      circles.splice(i);
+    }
+  }
+
+  /* HOMEWORK */
+  /* every two seconds you must create a circle in a random position. The radius, and color does not matter */
+
+  // if (circles[0].radius < radiusLimit){
+  //   circles[0].increaseSize()
+  // }
+
+  // if (circles[1].radius < radiusLimit){
+  //   circles[1].increaseSize()
+  // }
+  
+  // if (circles[2].radius < radiusLimit){
+  //   circles[2].increaseSize()
+  // }
+
+  // if (circles[3].radius < radiusLimit){
+  //   circles[3].increaseSize()
+  // }
+
+  // if (circles[4].radius < radiusLimit){
+  //   circles[4].increaseSize()
+  // }
+
+
+
+  // if (circle_1.radius < radiusLimit){
+  //   circle_1.increaseSize();
+  // }
+
+  
+
+
+
+
+  // if (dist(mouseX, mouseY, circle_1.xaxis, circle_1.yaxis) <= circle_1.radius){
+  //   console.log("hehexd");
+  // }
+  
+
+}
+
